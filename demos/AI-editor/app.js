7ed443b0568afce49321fc91ce86f839d1e8d80a
Victor Powell <vicapow@gmail.com>
2014-01-10 12:15:36 -0800

    updates to AI demo

--- demos/AI-editor/app.js
+++ demos/AI-editor/app.js
@@ -2,9 +2,11 @@
 
 var app = angular.module('myApp', [])
 
-app.controller('MainCtrl', function($scope){
+app.controller('MainCtrl', function($scope, $window){
   var attrHash = {}
-  
+
+
+  angular.element($window).on('resize', $scope.$apply.bind($scope))
   var ids = 0
   $scope.attributes = []
   $scope.addAttribute = function(attr){
@@ -33,8 +35,9 @@ app.controller('MainCtrl', function($scope){
   }
 
   $scope.players = []
+  var playerIds = 1
   $scope.addPlayer = function(){
-    $scope.players.push({ name: 'New Player', attributes: [] })
+    $scope.players.push({ name: 'Character ' + (playerIds++), attributes: [] })
     syncAttributes($scope.attributes)
   }
   $scope.removePlayer = function(){
@@ -76,40 +79,90 @@ app.controller('MainCtrl', function($scope){
 
 app.directive('radarChart', function(){
   function link(scope, el, attr){
-
-    var width = 200, height = 200
-    var radius = width / 2
+    var el = el[0]
+    var pi = Math.PI
+    var width = el.clientWidth, height = el.clientHeight
+    var radius = 0
+    var padding = 10
 
     // fiddle: http://jsfiddle.net/Wexcode/CrDUy/
     var angle = d3.scale.linear().range([0, Math.PI * 2])
 
+    var radiusFromDatum = function(d){
+      return (d.value - d.min) / (d.max - d.min) * radius 
+    }
+
     var line = d3.svg.line.radial()
       .interpolate("linear")
       .tension(0)
-      .radius(function(d){
-        return (d.value - d.min) / (d.max - d.min) * radius 
+      .radius(radiusFromDatum)
+      .angle(function(d, i) { return angle(i) })
+
+    var drag = d3.behavior.drag()
+    .on("drag", function(d,i) {
+      var self = this
+      scope.$apply(function(){
+        var x = d3.mouse(self)[0]
+        var p = x / radius
+        if(p > 1) p = 1
+        if(p < 0) p = 0
+        d.value =  p * (d.max - d.min)
+        d3.select(self).attr('cx', p * radius)
       })
-      .angle(function(d, i) { return angle(i); });
+    })
 
-    var svg = d3.select(el[0]).append('svg')
-      .attr({width: width, height: height})
-      .append('g')
-      .attr('transform', 'translate(' + width / 2 + ',' + height / 2 + ')')
+    var svg = d3.select(el).append('svg')
+    var g = svg.append('g')
+    var axis = g.selectAll('g.axis')
 
-    var area = svg.append('path')
+    var area = g.append('path')
       .attr('class', 'area')
       .style('opacity', '1')
       .style('fill', '#999')
+
+    var nobs = g.selectAll('g.nob')
 
     scope.$watch('data', function(data){
       if(!data) return
       angle.domain([0, data.length])
       area.datum(data).attr('d', line)
-      var axis = svg.selectAll('g.axis').data(data)
+
+      // add, update, or remove axis
+      axis = axis.data(data)
+      axis.enter().append('g')
+        .attr('class', 'axis').call(updateAxis)
+        .append('text').attr('class', 'label')
+          .style('text-anchor', 'end')
       axis.call(updateAxis)
-      axis.enter().append('g').attr('class', 'axis').call(updateAxis)
       axis.exit().remove()
+
+      // add, update, or remove nobs
+      nobs = nobs.data(data)
+      nobs.enter().append('g')
+        .attr('class', 'nob')
+        .append('circle').attr('r', 5)
+        .call(drag)
+      nobs.call(updateNobs)
+      nobs.exit().remove()
     }, true)
+
+    scope.$watch(function(){
+      return el.clientWidth * el.clientHeight
+    }, function(){
+      width = el.clientWidth
+      height = el.clientHeight
+      var newRadius = Math.min(width, height) / 2
+
+      // no need to run these updates if the radius didn't change
+      if(newRadius - padding !== radius){
+        radius = newRadius - padding
+        area.attr('d', line) // update the area to the new radius
+        axis.call(updateAxis)
+        nobs.call(updateNobs)
+      }
+      svg.attr({width: width, height: height})
+      g.attr('transform', 'translate(' + width / 2 + ',' + height / 2 + ')')
+    })
 
     function updateAxis(axis){
       axis.each(function(d, i){
@@ -122,8 +175,25 @@ app.directive('radarChart', function(){
             return 'rotate(' + (angle(i) / Math.PI * 180 - 90) + ')'
           })
       })
+      axis.call(updateAxisLabels)
     }
 
+    function updateAxisLabels(axis){
+      axis.select('text.label')
+        .attr('transform', 'translate(' + radius + ', -5)')
+        .text(function(d){ return d.name })
+    }
+
+    function updateNobs(nobs){
+      nobs.attr('transform', function(d, i){
+          return 'rotate(' + (angle(i) / Math.PI * 180 - 90) + ')'
+        })
+        .select('circle').attr('cx', radiusFromDatum)
+    }
+  }
+
+  function pointFromAngleRadius(angle, radius){
+    return [ Math.cos(angle) * radius, Math.sin(angle) * radius ]
   }
   return {
     restrict: 'E',
